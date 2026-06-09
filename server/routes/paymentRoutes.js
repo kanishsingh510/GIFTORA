9123985733eafc5a08d9b96b3179c65a3a1b59c8
siddhantkumar101 <kumarsiddhant815@gmail.com>
2026-06-09 15:19:23 +0530

    Remove backend server folder from Git tracking - keep local only

--- server/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createPaymentOrder, verifyPayment } from "../controllers/paymentController.js";
-
-const router = express.Router();
-
-router.post("/create-order", createPaymentOrder);
-router.post("/verify", verifyPayment);
-
-export default router;
